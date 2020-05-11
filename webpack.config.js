94f780a534bab2cdb7bf8825396a23b6e9ef8a87
DVo1 <dimytr.voitenko@gmail.com>
2020-05-11 19:26:13 +0300

    test: test new config from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,7 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: './index.js',
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: 'template.html',
-    }),
-    new CleanWebpackPlugin(),
-  ],
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
@@ -26,5 +12,13 @@ module.exports = {
         },
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
   },
 };
