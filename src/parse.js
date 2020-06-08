e73e91352f1e235c1f2f3b4e6b70f79360374fa4
DVo1 <dimytr.voitenko@gmail.com>
2020-06-08 18:03:54 +0300

    refactor: rewritten in declarative style

--- src/parse.js
+++ src/parse.js
@@ -1,20 +1,16 @@
-/* eslint-disable implicit-arrow-linebreak */
-import { hasIn, uniqueId } from 'lodash';
-
 const parse = (data) => {
-  const id = uniqueId();
-  const items = [];
-  const iter = (coll) =>
-    [...coll.children].reduce((acc, node) => {
-      const { nodeName, children, textContent } = node;
-      const value = !children.length ? textContent : iter(node);
-      if (hasIn(children, nodeName)) {
-        items.push(value);
-        return { id, ...acc, [nodeName]: items };
-      }
-      return { id, ...acc, [nodeName]: value };
-    }, {});
   const document = new window.DOMParser().parseFromString(data, 'text/xml');
-  return iter(document);
+
+  return {
+    title: document.querySelector('title').textContent,
+    description: document.querySelector('description').textContent,
+    link: document.querySelector('link').textContent,
+    items: [...document.querySelectorAll('item')].map((item) => ({
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    })),
+  };
 };
+
 export default parse;
