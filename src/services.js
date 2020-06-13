ae3b000ee3b594a7aa6e27e260dbe8530f6ce328
DVo1 <dimytr.voitenko@gmail.com>
2020-06-14 01:23:12 +0300

    refactor: The function 'preparedContent' is removed because it is not needed

--- src/services.js
+++ src/services.js
@@ -1,38 +1,25 @@
-/* eslint-disable no-param-reassign */
-import axios from 'axios';
-import * as yup from 'yup';
-import i18next from 'i18next';
-import { differenceBy, uniqueId } from 'lodash';
-import parse from './parse';
-import resources from './locales';
+import axios from "axios";
+import * as yup from "yup";
+import i18next from "i18next";
+import { differenceBy, uniqueId } from "lodash";
+import parse from "./parse";
+import resources from "./locales";
 
 i18next.init({
-  lng: 'en',
+  lng: "en",
   resources,
 });
 
-const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 const makeProxyBasedURL = (url) => `${PROXY_URL}${url}`;
 
 export const validateURL = (url, feeds) =>
   yup
     .string()
-    .url(i18next.t('url'))
-    .required(i18next.t('required'))
-    .notOneOf(feeds, i18next.t('notOneOf'))
+    .url(i18next.t("url"))
+    .required(i18next.t("required"))
+    .notOneOf(feeds, i18next.t("notOneOf"))
     .validate(url);
-
-const preparedContent = (data, state, url) => {
-  const id = uniqueId();
-
-  const { title, description, link, items } = data;
-
-  const channel = { id, title, description, link, url };
-  state.data.feeds = [channel, ...state.data.feeds];
-
-  const news = items.map((item) => ({ id, ...item }));
-  state.data.news = [...news, ...state.data.news];
-};
 
 export const getFeedData = (state) => {
   const { url } = state.form.input;
@@ -41,32 +28,41 @@ export const getFeedData = (state) => {
   return axios
     .get(urlWithProxy)
     .then(({ data }) => parse(data))
-    .then((parsedData) => preparedContent(parsedData, state, url));
+    .then((parsedData) => {
+      const id = uniqueId();
+      const { title, description, link, items } = parsedData;
+
+      const channel = { id, title, description, link, url };
+      state.data.feeds = [channel, ...state.data.feeds];
+
+      const news = items.map((item) => ({ id, ...item }));
+      state.data.news = [...news, ...state.data.news];
+    });
 };
 
 export const updateFeedData = (state) => {
-  const feedsUrls = state.data.feeds.map(({ url }) => makeProxyBasedURL(url));
-
+  const { feeds, news } = state.data;
+  const feedsUrls = feeds.map(({ url }) => makeProxyBasedURL(url));
   const promiseUrls = feedsUrls.map(axios.get);
 
   return Promise.all(promiseUrls)
     .then((response) => response.map(({ data }) => parse(data)))
     .then((parsedData) => {
-      const oldChannels = state.data.feeds;
-      const updateChannels = parsedData.map((item) => item);
-      const newChannels = differenceBy(updateChannels, oldChannels, 'link');
-      state.data.feeds = [...newChannels, ...oldChannels];
+      const updatedFeeds = parsedData.map((item) => item);
 
-      const oldItems = state.data.news;
+      const newFeeds = differenceBy(updatedFeeds, feeds, "link");
+      state.data.feeds = [...newFeeds, ...feeds];
+
       const updatedItems = parsedData.flatMap(({ items, link }) => {
-        const currentChannel = state.data.feeds.filter((feed) => feed.link === link);
-        const { id } = currentChannel[0];
+        const currentFeed = feeds.filter((feed) => feed.link === link);
+        const { id } = currentFeed[0];
         return items.map((item) => ({ id, ...item }));
       });
-      const newItems = differenceBy(updatedItems, oldItems, 'link');
-      state.data.news = [...newItems, ...oldItems];
+
+      const newItems = differenceBy(updatedItems, news, "link");
+      state.data.news = [...newItems, ...news];
     })
     .finally(() => {
-      setTimeout(() => updateFeedData(state), 30000);
+      setTimeout(() => updateFeedData(state), 5000);
     });
 };
